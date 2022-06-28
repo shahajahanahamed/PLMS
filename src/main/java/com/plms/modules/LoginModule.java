b67e142655676041cdb72d18df46d425cb145299
shahajahanahamed <ahamedshahajahan@gmail.com>
2022-06-28 22:10:14 +0530

    Project Begin at 10.00 PM on 28th of June

---
+++ src/main/java/com/plms/modules/LoginModule.java
@@ -0,0 +1,23 @@
+package com.plms.modules;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginModule extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginModule.class.getResource("/com/plms/design/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 640, 440);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.example.plms {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.plms.controller to javafx.fxml;
+    exports com.plms.controller;
+    exports com.plms.modules;
+    opens com.plms.modules to javafx.fxml;
+}
