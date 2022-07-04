41bb477deb4950c19c5d6d4d9f44a6520def5645
shahajahanahamed <ahamedshahajahan@gmail.com>
2022-07-04 22:39:53 +0530

    July 3rd - 10.42PM Commit, Modified homepage

--- src/main/java/com/plms/controller/LoginController.java
+++ src/main/java/com/plms/controller/LoginController.java
@@ -1,41 +1,69 @@
 package com.plms.controller;
-
-import com.plms.modules.LoginModule;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
-    public LoginController() {
+    @FXML
+    private Label valLbl;
+    @FXML
+    private TextField usernameTB;
+    @FXML
+    private PasswordField passwordTB;
 
-    }
-    @FXML
-    private Label wrongLogIn;
-    @FXML
-    private TextField username;
-    @FXML
-    private PasswordField password;
-    public void userLogIn(ActionEvent event) throws IOException {
-        checkLogin();
+    private static Stage stage;
 
-    }
-    private void checkLogin() throws IOException {
-        LoginModule loginM = new LoginModule();
-        if(username.getText().equals("admin") && password.getText().equals("123")) {
-            wrongLogIn.setText("Success!");
-            loginM.changeScene("/com/plms/design/homepage-view.fxml");
-        }
-        else if(username.getText().isEmpty() && password.getText().isEmpty()) {
-            wrongLogIn.setText("Please enter username and password.");
-        }
-        else {
-            wrongLogIn.setText("Invalid username or password!");
-        }
+    public static Stage getStage() {
+        return stage;
     }
 
+    public static void setStage(Stage stage) {
+        LoginController.stage = stage;
+    }
 
+    public void doLogin(MouseEvent mouseEvent) {
+        userLogin("Admin","123");
+    }
+
+    public void userLogin(String username,String password){
+        try {
+            if(usernameTB.getText().equals(username) && passwordTB.getText().equals(password)){
+                valLbl.setVisible(true);
+                valLbl.setStyle("-fx-text-fill:Green; -fx-font-size: 14;");
+                valLbl.setText("Login Successful");
+                Thread.sleep(10000);
+                valLbl.setVisible(false);
+                loadScene("/com/plms/design/homepage-view.fxml");
+            } else if (usernameTB.getText().isEmpty() || passwordTB.getText().isEmpty()) {
+                valLbl.setText("Please enter Username and Password");
+                valLbl.setStyle("-fx-text-fill:Red; -fx-font-size: 18;");
+                //Thread.sleep(2000);
+                //valLbl.setVisible(false);
+            }else {
+                valLbl.setText("Invalid Username or Password");
+                valLbl.setStyle("-fx-text-fill:Red; -fx-font-size: 18;");
+                //Thread.sleep(2000);
+                //valLbl.setVisible(false);
+            }
+        }catch (Exception e){
+            System.out.println("Exception Occured: "+e);
+
+        }
+    }
+    public void loadScene(String fxml){
+        Parent pane = null;
+        try {
+            pane = FXMLLoader.load(Homepage.class.getResource(fxml));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.getScene().setRoot(pane);
+    }
 }
--- src/main/java/com/plms/modules/Main.java
+++ src/main/java/com/plms/modules/Main.java
@@ -2,6 +2,7 @@ package com.plms.modules;
 
 //import com.sun.javafx.tk.quantum.PaintRenderJob;
 import com.plms.controller.Homepage;
+import com.plms.controller.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,21 +11,19 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LoginModule extends Application {
-    private static Stage stg;
+public class Main extends Application {
+    private static Stage stage;
     @Override
     public void start(Stage primaryStage) throws IOException {
-        stg=primaryStage;
+        stage=primaryStage;
         primaryStage.setResizable(false);
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginModule.class.getResource("/com/plms/design/login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/plms/design/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        primaryStage.setTitle("Hello!");
+        primaryStage.setTitle("PLMS");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-    public void changeScene(String fxml) throws IOException {
-        Parent pane = FXMLLoader.load(Homepage.class.getResource(fxml));
-        stg.getScene().setRoot(pane);
+
+        LoginController.setStage(stage);
     }
     public static void main(String[] args) {
         launch();
