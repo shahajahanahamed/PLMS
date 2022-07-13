b3e5219e92e8787d253afd5092329683ccd0eafc
shahajahanahamed <ahamedshahajahan@gmail.com>
2022-07-13 22:11:40 +0530

    JULY 08- 12.40AM Commiting

--- src/main/java/com/plms/controller/AddEmployeeController.java
+++ src/main/java/com/plms/controller/AddEmployeeController.java
@@ -4,35 +4,59 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import com.plms.dao.EmployeeDao;
+import com.plms.entities.Employee;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.DatePicker;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AddEmployeeController implements Initializable {
     @FXML
-    private JFXTextArea addressTA;
+    private Button addBtn;
+
     @FXML
-    private JFXTextField contactTB;
+    private TextArea addressTA;
+
+    @FXML
+    private Button backBtn;
+
+    @FXML
+    private Button clearBtn;
+
+    @FXML
+    private TextField contactTB;
+
     @FXML
     private DatePicker dobDP;
+
     @FXML
-    private JFXTextField emailTB;
+    private TextField emailTB;
+
     @FXML
-    private JFXTextField fullnameTB;
+    private TextField fullnameTB;
+
     @FXML
-    private JFXComboBox<String> genderCB;
+    private ComboBox<String> genderCB;
+
     @FXML
-    private JFXPasswordField passwordTB;
+    private PasswordField passwordTB;
+
     @FXML
-    private JFXComboBox<String> typeCB;
+    private ComboBox<String> typeCB;
+
     @FXML
-    private JFXTextField usernameTB;
+    private TextField usernameTB;
+    @FXML
+    private Label validationLbl;
 
     private int empId;
-    private String empName;
+    private String empFullname;
     private String username;
     private String password;
     private String userType;
@@ -44,14 +68,55 @@ public class AddEmployeeController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        /*empName = fullnameTB.getText();
-        username=usernameTB.getText();
+        setValuesToTypeComboBox();
+        setValuesToGenderComboBox();
+    }
+
+    //Adding employee types to the Type Combobox
+    public void setValuesToTypeComboBox(){
+        ObservableList<String> employeeTypes = FXCollections.observableArrayList("Admin","Receptionist","Lab Technician","Others");
+        typeCB.getItems().setAll(employeeTypes);
+    }
+    public void setValuesToGenderComboBox(){
+        ObservableList<String> gender = FXCollections.observableArrayList("Male","Female","Others");
+        genderCB.getItems().setAll(gender);
+    }
+
+    @FXML
+    void clickOnAddButton(MouseEvent event) {
+        addEmployeeDetails();
+    }
+    public void addEmployeeDetails(){
+        //Getting values from fields
+        empFullname = fullnameTB.getText();
+        username = usernameTB.getText();
         password = passwordTB.getText();
         userType = typeCB.getValue();
+        dob = dobDP.getValue().toString();
         gender = genderCB.getValue();
         contactNo = contactTB.getText();
         emailId = emailTB.getText();
-        address = addressTA.getText();*/
+        address = addressTA.getText();
 
+        //adding to the employee object
+        Employee employee = new Employee();
+        employee.setEmpName(empFullname);
+        employee.setUsername(username);
+        employee.setPassword(password);
+        employee.setUserType(userType);
+        employee.setDob(dob);
+        employee.setGender(gender);
+        employee.setContactNo(contactNo);
+        employee.setEmailId(emailId);
+        employee.setAddress(address);
+
+        //
+        EmployeeDao empDao = new EmployeeDao();
+        int result = empDao.insertData(employee);
+        if(result==1){
+            validationLbl.setText("Employee Added Successfully");
+        }else {
+            validationLbl.setText("Employee Addition Failed");
+        }
     }
 }
--- src/main/java/com/plms/dao/EmployeeDao.java
+++ src/main/java/com/plms/dao/EmployeeDao.java
@@ -1,5 +1,6 @@
 package com.plms.dao;
 
+import com.plms.entities.Employee;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,11 +9,15 @@ public class EmployeeDao {
     private ApplicationContext context;
     private JdbcTemplate template;
 
-    EmployeeDao() {
+    public EmployeeDao() {
         context = new ClassPathXmlApplicationContext("/com/plms/config.xml");
         template = context.getBean("jdbcTemplate", JdbcTemplate.class);
+    }
 
-
+    public int insertData(Employee emp){
+        String sql = "insert into temployee (empName,username,password,userType,dob,gender,contactNo,emailId,address) values(?,?,?,?,?,?,?,?,?)";
+        int result = template.update(sql,emp.getEmpName(),emp.getUsername(),emp.getPassword(),emp.getUserType(),emp.getDob(),emp.getGender(),emp.getContactNo(),emp.getEmailId(),emp.getAddress());
+        return result;
     }
 
 }
